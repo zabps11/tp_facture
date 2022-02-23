8c5ddf2b03c8be2ed00132cdb372308dafd2e986
zabps11 <zgphilibert11@gmail.com>
2022-02-23 11:58:16 +0100

    program de ZANG BENGONO PHILIBERT STEPHANE premiere optimisatio

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 #L'objectif de ce mini TP consite en la génération d'une facture.
 import copy
-from math import prod
+
 productTemp={
     "name":"",
     "price":0,
@@ -11,22 +11,34 @@ productTemp={
 productList=[]
 
 
-print("_____Entrer liste e produit_______")
+print("_____Entrer liste de produit_______")
 newProduct=""
-while newProduct != "n":
-    productTemp["name"]= input("==> Name :  ")
-    productTemp["price"]= float(input("    Price :  "))
-    productTemp["quantity"]= int(input("    Quantity :  "))
-    newProduct=input("Enter a new product ? y/n__  ")
-    curentProduct=copy.deepcopy(productTemp)
-    productList.append(curentProduct)
-    print("----------------------------------------------")
-    while newProduct!="y" and newProduct!= "n":
-        newProduct=input("Enter a new product ? y/n")
+newFacture=""
+while newFacture != "n":
+
+    while newProduct != "n":
+        productTemp["name"]= input("==> Product Name :  ")
+        productTemp["price"]= float(input("    Product Price :  "))
+        productTemp["quantity"]= int(input("    Product Quantity :  "))
+        newProduct=input("Enter a new product ? y/n__  ")
+        curentProduct=copy.deepcopy(productTemp)
+        productList.append(curentProduct)
+        while newProduct!="y" and newProduct!= "n":
+            newProduct=input("Enter a new product ? y/n")
+        print("----------------------------------------------")
     
-totalPrice=0
-print("_______________________FACTURE_________________________")
-for product in productList:
-    print("     Name : {}    Quantity : {}  Price :  {}".format(product["name"],product["quantity"],product["price"]))
-    totalPrice=totalPrice + product["quantity"]*product["price"]
-print("     TOTAL : {}".format(totalPrice))
+    newProduct=""
+    totalPrice=0
+    print("_______________________FACTURE_________________________")
+    for product in productList:
+        print("     Name : {}    Quantity : {}  Price :  {}".format(product["name"],product["quantity"],product["price"]))
+        totalPrice=totalPrice + product["quantity"]*product["price"]
+    print("     TOTAL : {}".format(totalPrice))
+    print(" ")
+    newProduct = ""
+    productList.clear()
+
+    newFacture=input("Entrer liste e produit ? y/n__  ")
+    while newFacture!="y" and newFacture!= "n":
+        newFacture=input("Entrer liste e produit ? y:yes or n:no__  ")
+    print("----------------------------------------------")
